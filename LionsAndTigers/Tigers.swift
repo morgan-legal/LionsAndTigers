5b79f2e2629c330a76d1873a57b01fd9cfa945c9
morgan-legal <legal.morgan@gmail.com>
2015-04-22 18:13:44 -0700

    first commit - with transition view updated

---
+++ LionsAndTigers/Tigers.swift
@@ -0,0 +1,18 @@
+//
+//  Tigers.swift
+//  LionsAndTigers
+//
+//  Created by Morgan Le Gal on 4/22/15.
+//  Copyright (c) 2015 Morgan Le Gal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+    
+}
